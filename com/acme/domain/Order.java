42e81bffe7a75cca679abcba1eb99764c5272500
alabh <alabenhmouda@gmail.com>
2021-03-09 14:21:55 +0100

    made up to lab8

---
+++ com/acme/domain/Order.java
@@ -0,0 +1,115 @@
+package com.acme.domain;
+
+import com.acme.utils.MyDate;
+
+public class Order {
+    private static double taxRate;
+    private MyDate orderDate;
+    private double orderAmount = 0.00;
+    private String customer;
+    private String product;
+    private int quantity;
+
+    static { taxRate = 0.05; }
+
+    public MyDate getOrderDate() { return orderDate; }
+
+    public void setOrderDate(MyDate orderDate) { this.orderDate = orderDate; }
+
+    public double getOrderAmount() { return orderAmount; }
+
+    public void setOrderAmount(double orderAmount) {
+        if (orderAmount <= 0) {
+            System.out.println(
+                "Attempting to set the orderAmount to a value less than or equal to zero");
+        } else
+            this.orderAmount = orderAmount;
+    }
+
+    public String getCustomer() { return customer; }
+
+    public void setCustomer(String customer) { this.customer = customer; }
+
+    public String getProduct() { return product; }
+
+    public void setProduct(String product) { this.product = product; }
+
+    public int getQuantity() { return quantity; }
+
+    public void setQuantity(int quantity) {
+        if (quantity <= 0) {
+            System.out.println(
+                "Attempting to set the quantity to a value less than or equal to zero");
+        } else
+            this.quantity = quantity;
+    }
+    public static void setTaxRate(double newRate) { taxRate = newRate; }
+    public static double getTaxRate() { return taxRate; }
+
+    public Order(MyDate d, double amt, String c, String p, int q) {
+        if (amt <= 0) {
+            System.out.println(
+                "Attempting to set the orderAmount to a value less than or equal to zero");
+            return;
+        }
+        if (q <= 0) {
+            System.out.println(
+                "Attempting to set the quantity to a value less than or equal to zero");
+            return;
+        }
+        orderDate = d;
+        orderAmount = amt;
+        customer = c;
+        product = p;
+        quantity = q;
+    }
+
+    public Order(MyDate d, double amt, String c) {
+        this(d, amt, c, "Anvil", 1);
+    }
+
+    public String toString() {
+        return quantity + " ea. " + product + " for " + customer;
+    }
+
+    public static void computeTaxOn(double anAmount) {
+        System.out.println("The tax for " + anAmount +
+                           " is: " + anAmount * Order.taxRate);
+    }
+
+    public double computeTax() {
+        System.out.println("The tax for this order is: " +
+                           orderAmount * Order.taxRate);
+        return orderAmount * Order.taxRate;
+    }
+
+    public char jobSize() {
+        if (quantity <= 25) {
+            return 'S';
+        } else if (quantity <= 75) {
+            return 'M';
+        } else if (quantity <= 150) {
+            return 'L';
+        } else {
+            return 'X';
+        }
+    }
+
+    public double computeTotal() {
+        double total = orderAmount;
+        switch (jobSize()) {
+        case 'M':
+            total -= 0.01 * orderAmount;
+            break;
+        case 'L':
+            total -= 0.02 * orderAmount;
+            break;
+        case 'X':
+            total -= 0.03 * orderAmount;
+            break;
+        }
+        if (orderAmount <= 1500)
+            total += computeTax();
+        return total;
+    }
+}
---
+++ com/acme/utils/MyDate.java
@@ -0,0 +1,81 @@
+package com.acme.utils;
+
+public class MyDate {
+    private int day;
+    private int year;
+    private int month;
+
+    public MyDate() { this(1, 1, 1900); }
+
+    public MyDate(int m, int d, int y) {
+        if (valid(m, d, y)) {
+            month = m;
+            day = d;
+            year = y;
+        }
+    }
+
+    public int getMonth() { return month; }
+
+    public void setMonth(int month) {
+        if (valid(month, day, year)) {
+            this.month = month;
+        }
+    }
+
+    public int getYear() { return year; }
+
+    public void setYear(int year) {
+
+        if (valid(month, day, year)) {
+            this.year = year;
+        }
+    }
+
+    public int getDay() { return day; }
+
+    public void setDay(int day) {
+        if (valid(month, day, year)) {
+            this.day = day;
+        }
+    }
+
+    private boolean valid(int month, int day, int year) {
+        if (day > 31 || day < 1 || month > 12 || month < 1) {
+
+            System.out.println("Attempting to create a non-valid date " +
+                               month + "/" + day + "/" + year);
+            return false;
+        }
+        switch (month) {
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            return (day <= 30);
+        case 2:
+            return day <= 28 || (day == 29 && year % 4 == 0);
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return day + "/" + month + "/" + year;
+    }
+
+    public void setDate(int m, int d, int y) {
+        if (valid(m, d, y)) {
+            month = m;
+            day = d;
+            year = y;
+        }
+    }
+
+    public static void leapYears() {
+        for (int i = 1752; i <= 2020; i++) {
+            if ((i % 4 == 0) && ((i % 100 != 0) || (i % 400 == 0)))
+                System.out.println("The year " + i + " is a leap year");
+        }
+    }
+}
